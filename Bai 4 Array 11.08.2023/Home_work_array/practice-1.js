59af488e87a635f322fae681956430a350998b73
Heillo212 <134430103+Heillo212@users.noreply.github.com>
2023-11-08 16:55:52 +0700

    Add files via upload

---
+++ Bai 4 Array 11.08.2023/Home_work_array/practice-1.js
@@ -0,0 +1,8 @@
+// Bài 1: Hãy viết một chương trình để đảo ngược các phần tử trong list(array)
+let aList = [100, 200, 300, 400, 500];
+
+let reversedList = aList.reverse();
+
+
+console.log([reversedList]);
+// Output: [500, 400, 300, 200, 100]
