f4a6643357c5ffcaf391c2b1975d48be21a608ad
Heillo212 <134430103+Heillo212@users.noreply.github.com>
2023-11-08 16:54:10 +0700

    Add files via upload

---
+++ Bai 4 Array 11.08.2023/Home_work_array/practice-3.js
@@ -0,0 +1,15 @@
+// Bài 3: Cho sẵn một mảng  chứa các số tự nhiên. 
+// Bình phương các phần tử trong mảng và lưu vào một mảng  mới.
+// let array1 = [1, 2, 3, 4, 5, 6, 7];
+// let array2 = [];
+
+// console.log(array2); // Output: [1, 4, 9, 16, 25, 36, 49]
+
+let array1 = [1, 2, 3, 4, 5, 6, 7];
+let array2 = [];
+
+for (let i = 0; i < array1.length; i++) {
+  array2.push(array1[i] ** 2);
+}
+
+console.log(array2);  // Output: [1, 4, 9, 16, 25, 36, 49]
